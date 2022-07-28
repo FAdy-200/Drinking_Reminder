2cf5b44355e641c68d8d792e684434fcf9c0e3dc
FAdy-200 <fadi.alahmad.alomar@gmail.com>
2022-07-28 17:02:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# drinking_reminder
+
+A simple app to remind people to drink water, and track their consumption.
+
---
+++ android/app/src/main/kotlin/com/FAdy200/drinking_reminder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.FAdy200.drinking_reminder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
